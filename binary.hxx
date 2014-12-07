4a025a2bbe4ee6da2ee9efc7d27d3f18ff387365
Salvador Guzman <guzmansalv@gmail.com>
2014-12-07 05:43:24 -0800

    added to whole despairagus interproject namespace

--- binary.hxx
+++ binary.hxx
@@ -4,110 +4,113 @@
 #include <cstring>
 #include <iostream>
 
-namespace bitwise {
-	using std::cout;
-	using std::ostream;
+namespace desparaigus {
+	namespace bitwise {
+		using std::cout;
+		using std::ostream;
 
-	template <typename A>
-	class binary final {
-		template <typename B>
-		using conref = const B&;
+		template<typename A>
+		class binary final {
+			template<typename B>
+			using conref = const B &;
 
-		constexpr static size_t sz{sizeof(A)};
-		constexpr static size_t szB{sz << 3};
+			constexpr static size_t sz{sizeof(A)};
+			constexpr static size_t szB{sz << 3};
 
-		using bitA = std::bitset<szB>;
-		using byte = unsigned char;
-		using bit = bool;
+			using bitA = std::bitset<szB>;
+			using byte = unsigned char;
+			using bit = bool;
 
-	public:
-		explicit binary(void) = delete;
+		public:
+			explicit binary(void) = delete;
 
-		explicit binary(conref<A> a) noexcept {
-			byte* ptr = new byte[sz];
-			memcpy((void *) ptr, (const void* const) &a, sz);
+			explicit binary(conref<A> a) noexcept {
+				byte *ptr = new byte[sz];
+				memcpy((void *) ptr, (const void *const) &a, sz);
 
-			for (size_t i = 0; i < sz; ++i) {
-				static byte ch;
-				ch = ptr[i];
-				for (size_t j = 0; j < 8; ++j) {
-					static bool bitVal;
-					bitVal = binary<A>::getBitFromByte(ch, j);
-					this->bits[binary<A>::getBitIdx(i, j)] = bitVal;
+				for (size_t i = 0; i < sz; ++i) {
+					static byte ch;
+					ch = ptr[i];
+					for (size_t j = 0; j < 8; ++j) {
+						static bool bitVal;
+						bitVal = binary<A>::getBitFromByte(ch, j);
+						this->bits[binary<A>::getBitIdx(i, j)] = bitVal;
+					}
+				}
+
+				delete[] ptr;
+			}
+
+			explicit binary(A &&a) noexcept : binary(a) {
+			}
+
+			template<typename... B>
+			explicit binary(B... b) = delete;
+
+			inline ~binary(void) noexcept {
+				bits.~bitA();
+			}
+
+			inline void print(void) const noexcept {
+				binary<A>::print(std::cout, *this);
+			}
+
+			inline bool getBit(conref<size_t> idx) const noexcept {
+				return bits[idx];
+			}
+
+			inline bool getBit(conref<byte> byteIdx, conref<byte> bitIdx) const noexcept {
+				return binary<A>::getBit(*this, byteIdx, bitIdx);
+			}
+
+			template<typename... B>
+			inline bool getBit(B... b) const noexcept = delete;
+
+			template<typename B>
+			friend inline ostream &operator<<(ostream &os, conref<binary<B>> a) noexcept {
+				a.print();
+
+				return os;
+			}
+
+			template<typename B>
+			friend inline ostream &operator<<(ostream &os, binary<B> &&a) noexcept {
+				a.print();
+
+				return os;
+			}
+
+		private:
+			static inline void print(std::ostream &os, conref<binary<A>> a) noexcept {
+				constexpr static size_t szOneLess{sz - 1};
+
+				for (size_t i = 0; i < sz; ++i) {
+					for (size_t j = 0; j < 8; ++j) {
+						static bit bitIdx;
+						bitIdx = (bit) a.bits[getBitIdx(i, j)];
+						os << bitIdx;
+					}
+
+					if (szOneLess != i) {
+						os << ' ';
+					}
 				}
 			}
 
-			delete [] ptr;
-		}
 
-		explicit binary(A&& a) noexcept : binary(a) {}
-
-		template <typename... B>
-		explicit binary(B... b) = delete;
-
-		inline ~binary(void) noexcept{
-			bits.~bitA();
-		}
-
-		inline void print(void) const noexcept {
-			binary<A>::print(std::cout, *this);
-		}
-
-		inline bool getBit(conref<size_t> idx) const noexcept {
-			return bits[idx];
-		}
-
-		inline bool getBit(conref<byte> byteIdx, conref<byte> bitIdx) const noexcept {
-			return binary<A>::getBit(*this, byteIdx, bitIdx);
-		}
-
-		template <typename... B>
-		inline bool getBit(B... b) const noexcept = delete;
-
-		template <typename B>
-		friend inline ostream& operator<<(ostream& os, conref<binary<B>> a) noexcept {
-			a.print();
-
-			return os;
-		}
-
-		template <typename B>
-		friend inline ostream& operator<<(ostream& os, binary<B>&& a) noexcept {
-			a.print();
-
-			return os;
-		}
-
-	private:
-		static inline void print(std::ostream& os, conref<binary<A>> a) noexcept {
-			constexpr static size_t szOneLess{sz - 1};
-
-			for (size_t i = 0; i < sz; ++i) {
-				for (size_t j = 0; j < 8; ++j) {
-					static bit bitIdx;
-					bitIdx = (bit) a.bits[getBitIdx(i, j)];
-					os << bitIdx;
-				}
-
-				if (szOneLess != i) {
-					os << ' ';
-				}
+			static inline bit getBitFromByte(conref<byte> data, conref<byte> bitIdx) noexcept {
+				return data & (1 << bitIdx);
 			}
-		}
 
+			static inline size_t getBitIdx(conref<size_t> byteIdx, conref<size_t> bitIdx) noexcept {
+				return (byteIdx << 3) + bitIdx;
+			}
 
-		static inline bit getBitFromByte(conref<byte> data, conref<byte> bitIdx) noexcept {
-			return data & (1 << bitIdx);
-		}
+			static inline bit getBit(conref<binary<A>> bits, conref<size_t> byteIdx, conref<size_t> bitIdx) noexcept {
+				return bits[getBitIdx(byteIdx, bitIdx)];
+			}
 
-		static inline size_t getBitIdx(conref<size_t> byteIdx, conref<size_t> bitIdx) noexcept {
-			return (byteIdx << 3) + bitIdx;
-		}
-
-		static inline bit getBit(conref<binary<A>> bits, conref<size_t> byteIdx, conref<size_t> bitIdx) noexcept {
-			return bits[getBitIdx(byteIdx, bitIdx)];
-		}
-
-		bitA bits;
-	};
+			bitA bits;
+		};
+	}
 }
