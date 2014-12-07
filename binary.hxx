5b5d54ecabf5f8aa2cacb4851ca4149b8e8d381d
Salvador Guzman <guzmansalv@gmail.com>
2014-12-07 00:06:34 -0800

    constructors & destructors for binary

--- binary.hxx
+++ binary.hxx
@@ -1,7 +1,20 @@
 #include <bitset>
 
 template <typename A>
+using conref = const A&;
+
+template <typename A>
 class binary final {
 public:
+	explicit binary(void) = null;
+
+	explicit binary(conref<A> a);
+
+	explicit binary(A&& a);
+
+	template <typename... B>
+	explicit binary(B... b) = delete;
+
+	~binary(void);
 private:
 };
