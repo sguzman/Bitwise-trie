cf2445fa287d7bf3463eb6c4925f53f9d3c83036
Salvador Guzman <guzmansalv@gmail.com>
2014-12-10 15:59:21 -0800

    adding stub for bitwise trie

---
+++ bitwisetrie.hxx
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <iostream>
+
+#include "binary.hxx"
+
+namespace despairagus {
+	namespace bitwisetrie {
+		using std::cout;
+		using std::ostream;
+
+		template <typename A>
+		class bitwisetrie {
+			explicit bitwisetrie(void) = delete;
+		};
+	}
+}
