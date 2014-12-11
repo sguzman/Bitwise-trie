86acc96b6f27ce17af4de0f9f804ebf2e3a6d8ae
Salvador Guzman <guzmansalv@gmail.com>
2014-12-10 18:41:56 -0800

    pointers for children node

--- bitnode.hxx
+++ bitnode.hxx
@@ -4,8 +4,11 @@ namespace despairagus {
 	namespace bitnode {
 		template <typename A>
 		class bitnode {
+			explicit bitnode(void) : zero{nullptr}, one{nullptr} {}
 		public:
 		private:
+			bitnode* zero;
+			bitnode* one;
 		};
 	}
 }
