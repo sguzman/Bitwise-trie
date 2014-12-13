24512110d22c7476194ce65b0b285cf43aae3de1
Salvador Guzman <guzmansalv@gmail.com>
2014-12-12 23:06:02 -0800

    make unnamed namespace for decls

--- bitnode.hxx
+++ bitnode.hxx
@@ -4,11 +4,27 @@ namespace despairagus {
 	namespace bitnode {
 		template <typename A>
 		class bitnode {
-			template<typename B>
-			using conref = const B &;
+			namespace {
+				template<typename B>
+				using conref = const B &;
 
-			using byte = unsigned char;
-			using bit = bool;
+				using byte = unsigned char;
+				using bit = bool;
+
+				template <typename A>
+				static inline void killPtr(A* &a) noexcept {
+					if (a != nullptr) {
+						delete a;
+						a = nullptr;
+					}
+				}
+
+				template <>
+				static inline void killPtr(A) noexcept;
+
+				template <>
+				static inline void killPtr(bitnode<A>) noexcept;
+			}
 
 		public:
 			inline explicit bitnode(void) noexcept : zero{nullptr}, one{nullptr}, data{nullptr} {}
